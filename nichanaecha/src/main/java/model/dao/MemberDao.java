848cebf2eb121eaa5dd9ac73cca720ced83eafb8
504 <504@DESKTOP-FDBPKQC>
2023-09-15 17:42:48 +0900

    이성호 0915/17:42 커밋커밋!

--- nichanaecha/src/main/java/model/dao/MemberDao.java
+++ nichanaecha/src/main/java/model/dao/MemberDao.java
@@ -1,7 +1,93 @@
 package model.dao;
 
+import model.dto.MemberDto;
+
 public class MemberDao extends Dao {
 	private static MemberDao memberDao = new MemberDao();
 	public static MemberDao getInstence() {return memberDao;}
 	private MemberDao() {}
+	
+	// 1. 회원가입
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// 2. 로그인
+	public boolean login( String mid, String mpwd) {
+		try {
+			String sql = "";
+		
+			
+		}catch (Exception e) {System.out.println(e);}
+		return false;
+	}
+	
+	
+	// 3. 아이디 찾기 
+	
+	
+	// 4. 비밀번호 찾기
+	
+	
+	// 5. 내정보 호출 
+		public MemberDto info( String mid ) {
+			return null;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
