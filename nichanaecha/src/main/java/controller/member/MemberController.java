cfdecd8339afa7a170dc12e397b55d93a399fbf3
504 <504@DESKTOP-GR47Q4P>
2023-09-15 17:41:05 +0900

    230915_김규리회원페이지 다오연결 17:40 취합

--- nichanaecha/src/main/java/controller/member/MemberController.java
+++ nichanaecha/src/main/java/controller/member/MemberController.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.dao.MemberDao;
+import model.dto.MemberDto;
+
 /**
  * Servlet implementation class MemberController
  */
@@ -25,8 +30,19 @@ public class MemberController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 규리 마이페이지 출력
+		// System.out.println("컨트롤러연결");
+		int loginDto = 2; //임시테스트
+		request.getSession().setAttribute("loginDto", loginDto ); //임시테스트 세션에 로그인회원번호넣어놓기
+		//System.out.println("loginDto : "+loginDto);
+		int dto = (int) request.getSession().getAttribute("loginDto");
+		//System.out.println("로그인 세션 상태 : "+dto);
+		MemberDto result = MemberDao.getInstence().mview( dto );
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		String json = objectMapper.writeValueAsString(result);
+		response.setContentType("application/json;charset=UTF-8");
+    	response.getWriter().print(json);
 	}
 
 	/**
--- nichanaecha/src/main/java/model/dao/MemberDao.java
+++ nichanaecha/src/main/java/model/dao/MemberDao.java
@@ -1,7 +1,15 @@
 package model.dao;
 
+import model.dto.MemberDto;
+
 public class MemberDao extends Dao {
 	private static MemberDao memberDao = new MemberDao();
 	public static MemberDao getInstence() {return memberDao;}
 	private MemberDao() {}
+	
+	
+	//규리
+	public MemberDto mview( int dto) {
+		return null;
+	}
 }
