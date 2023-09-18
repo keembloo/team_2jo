22a929e8caaaa7da46f09afd9593ee4eb5bde89a
504 <504@DESKTOP-FDBPKQC>
2023-09-18 20:42:36 +0900

    이성호 - 등록페이지 0918/20:42

--- nichanaecha/src/main/java/model/dto/AuctionDto.java
+++ nichanaecha/src/main/java/model/dto/AuctionDto.java
@@ -1,5 +1,64 @@
 package model.dto;
 
 public class AuctionDto {
-
+	// 필드
+	String btitle;
+	String bprice;
+	String bfile;
+	int mno;
+	int bcno;
+	
+	//생성자
+	public AuctionDto() {}	//빈생성자
+		//풀생성자
+		public AuctionDto(String btitle, String bprice, String bfile, int mno, int bcno) {
+			super();
+			this.btitle = btitle;
+			this.bprice = bprice;
+			this.bfile = bfile;
+			this.mno = mno;
+			this.bcno = bcno;
+		}
+		
+	
+	//메소드
+		public String getBtitle() {
+			return btitle;
+		}
+		public void setBtitle(String btitle) {
+			this.btitle = btitle;
+		}
+		public String getBprice() {
+			return bprice;
+		}
+		public void setBprice(String bprice) {
+			this.bprice = bprice;
+		}
+		public String getBfile() {
+			return bfile;
+		}
+		public void setBfile(String bfile) {
+			this.bfile = bfile;
+		}
+		public int getMno() {
+			return mno;
+		}
+		public void setMno(int mno) {
+			this.mno = mno;
+		}
+		public int getBcno() {
+			return bcno;
+		}
+		public void setBcno(int bcno) {
+			this.bcno = bcno;
+		}
+		
+		
+		@Override
+		public String toString() {
+			return "AuctionDto [btitle=" + btitle + ", bprice=" + bprice + ", bfile=" + bfile + ", mno=" + mno
+					+ ", bcno=" + bcno + "]";
+		}
+	
+		
 }
--- nichanaecha/src/main/webapp/auction/carsubmit.jsp
+++ nichanaecha/src/main/webapp/auction/carsubmit.jsp
@@ -10,9 +10,27 @@
 <body>
 	<!-- jsp  파일안에 다른 jsp파일 import -->
 	<%@include file ="../header.jsp" %>
-
-
-
+	<div class="webcontainer">
+	<h3>등록페이지</h3>
+		<form class="carsubmitForm">
+			<div>
+				<div>
+					제목 : 		<input type="text" name="btitle" class="btitle"> <br/>
+					가격 : 		<input type="text" name="bprice" class="bprice"> <br/>
+					첨부파일 : 	<input type="file" name="bfile" class="bfile"> <br/>
+				</div>
+				<div>
+					<!-- 첨부파일에 사진 첨부하면 사진 보여지는 구간  -->
+				</div>
+			</div>
+			본문 : 		<textarea name="bcontent" class="bcontent" rows="" cols=""></textarea>	<br/>
+			<div>
+				<button onclick="bcarsubmit()">등록</button>
+				<button>취소</button>
+			</div>
+		</form>
+	</div>
+	<script src="../js/auction/carsubmit.js" type="text/javascript"></script>
 	<!-- jsp  파일안에 다른 jsp파일 import -->
 	<%@include file ="../footer.jsp" %>
 </body>
